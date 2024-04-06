d75dd598b037c6ab0b649bb56ad6164993fabbdd
5-23 <yhanbyeol6@gmail.com>
2024-04-06 16:49:56 +0900

    fix: multiple create bug

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use tosspay::TossPay;
 #[tokio::main]
 async fn main() {
     let toss = TossPay::new("wntjd0612".to_string());
+    let toss2 = TossPay::new("wntjd0612".to_string());
     let trace = toss.trace_all().await;
     loop {
         let recv = trace.recv();
--- tosspay/src/lib.rs
+++ tosspay/src/lib.rs
@@ -1,11 +1,46 @@
 use std::{sync::mpsc, thread};
-
+static IDS: Vec<String> = Vec::new();
+static mut RES: TossPayJson = TossPayJson {
+    resultType: String::new(),
+    success: TossPaySuccess {
+        nextCursor: String::new(),
+        data: Vec::new(),
+    },
+};
+static mut IS_RUNNING: bool = false;
 #[derive(Clone)]
 pub struct TossPay {
     toss_id: String,
 }
 impl TossPay {
     pub fn new(toss_id: String) -> Self {
+        if unsafe { !IS_RUNNING } {
+            panic!("Can't create multiple tosspay!");
+        }
+        unsafe { IS_RUNNING = true }
+        let toss_id_clone = toss_id.clone();
+        tokio::spawn(async move {
+            loop {
+                let toss_response = reqwest::Client::new()
+                    .get(format!("https://api-public.toss.im/api-public/v3/cashtag/transfer-feed/received/list?inputWord={}", toss_id_clone));
+                let json = serde_json::from_str::<TossPayJson>(
+                    &toss_response
+                        .send()
+                        .await
+                        .unwrap()
+                        .text()
+                        .await
+                        .unwrap()
+                        .replace("null", "\"\""),
+                )
+                .unwrap();
+                unsafe {
+                    RES = json;
+                }
+                thread::sleep(std::time::Duration::from_secs(2));
+            }
+        });
+
         TossPay { toss_id }
     }
     pub async fn trace_all(&self) -> mpsc::Receiver<TossPayData> {
@@ -14,7 +49,7 @@ impl TossPay {
         let mut old_datas = Vec::new();
         tokio::spawn(async move {
             loop {
-                let json = fake_self.request().await;
+                let json = unsafe { RES.clone() };
                 let datas = json.success.data;
                 if old_datas.clone() != datas.clone() {
                     let send_data = datas.clone();
@@ -27,27 +62,11 @@ impl TossPay {
                     // let send_data = datas.clone().iter().filter(|e| old_datas.clone().contains(e));
                 }
                 old_datas = datas.clone();
-                thread::sleep(std::time::Duration::from_secs(3));
+                // let rng = rand
+                thread::sleep(std::time::Duration::from_secs(2));
             }
         });
         receiver
-    }
-
-    async fn request(&self) -> TossPayJson {
-        let toss_response = reqwest::Client::new()
-            .get(format!("https://api-public.toss.im/api-public/v3/cashtag/transfer-feed/received/list?inputWord={}", self.toss_id));
-        let json = serde_json::from_str::<TossPayJson>(
-            &toss_response
-                .send()
-                .await
-                .unwrap()
-                .text()
-                .await
-                .unwrap()
-                .replace("null", "\"\""),
-        )
-        .unwrap();
-        json
     }
 }
 
